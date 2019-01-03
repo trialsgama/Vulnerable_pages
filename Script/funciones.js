5091b0f072514847ec7fda6883acd79db7d989fb
Estrella <leandro.e.estrella@accenture.com>
2019-01-03 12:40:50 -0300

    Luchando con esto

--- Script/funciones.js
+++ Script/funciones.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
 
     $('#buscar').onclick(function(e) {
+        src="https://www.googleapis.com/customsearch/v1?key=AIzaSyDXW6DrmLJZR6EnXtvTgeS4Q1BCXH46bqE&cx=017576662512468239146:omuauf_lfve&q=¨" + $('#query').val() + "&callback=hndlr"
 
-
+        function hndlr(response) {
+            for (var i = 0; i < response.items.length; i++) {
+              var item = response.items[i];
+              // in production code, item.htmlTitle should have the HTML entities escaped.
+              document.getElementById("content").innerHTML += "<br>" + item.htmlTitle;
+            }
+          }
     });
 
 
